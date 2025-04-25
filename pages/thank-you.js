2c7dcd156a81b13bc54d887687b40a05fa4f0ce3
GrowPilotuk <matthewcierpiol@gmail.com>
2025-04-25 21:22:15 +0100

    Update thank-you.js

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,1 +1,9 @@
+export default function ThankYou() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-green-50 text-center p-8">
+      <h1 className="text-4xl font-bold mb-4 text-green-800">Thank You!</h1>
+      <p className="text-lg text-gray-700">We have received your order. We'll be in touch shortly!</p>
+    </div>
+  );
+}
 // Thank you page
