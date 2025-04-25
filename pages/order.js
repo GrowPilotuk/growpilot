e90fd9ace9809b95d722ce6bf31002f020a70249
GrowPilotuk <matthewcierpiol@gmail.com>
2025-04-25 21:22:00 +0100

    Update order.js

--- pages/order.js
+++ pages/order.js
@@ -1,1 +1,9 @@
+export default function Order() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-green-50 text-center p-8">
+      <h1 className="text-4xl font-bold mb-4 text-green-800">Order Your Grow Room Plan</h1>
+      <p className="text-lg text-gray-700">Order page is under construction. Coming soon!</p>
+    </div>
+  );
+}
 // Order form page
