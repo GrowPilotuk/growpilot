6c05b01926389c3f86983432a01bc95481bff893
GrowPilotuk <matthewcierpiol@gmail.com>
2025-04-25 21:30:38 +0100

    Delete pages directory

--- pages/order.js
+++
@@ -1,9 +0,0 @@
-export default function Order() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-center p-8">
-      <h1 className="text-5xl font-extrabold mb-6">Order Your Plan</h1>
-      <p className="text-lg">Coming soon: choose your package and customize your grow room!</p>
-    </div>
-  );
-}
-// Order form page
--- pages/thank-you.js
+++
@@ -1,8 +0,0 @@
-export default function ThankYou() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-center p-8">
-      <h1 className="text-5xl font-extrabold mb-6">Thank You!</h1>
-      <p className="text-lg">We've received your order. Our team will be in touch shortly!</p>
-    </div>
-  );
-}
