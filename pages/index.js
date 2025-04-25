2f4b16bd12c0592e0513f09570c91ccbe23aaa03
GrowPilotuk <matthewcierpiol@gmail.com>
2025-04-25 21:14:17 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-100">
-      <h1 className="text-4xl font-bold">GrowPilot Landing is working!</h1>
-    </div>
-  )
-}
